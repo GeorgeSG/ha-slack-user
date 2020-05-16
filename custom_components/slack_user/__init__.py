20620b21053e75a47e5cada437e41f904ca29a35
Georgi Gardev <georgi@gardev.com>
2020-05-17 02:49:40 +0300

    Remove unused imports

--- custom_components/slack_user/__init__.py
+++ custom_components/slack_user/__init__.py
@@ -1,13 +1,5 @@
 """Slack User Component."""
 
-import asyncio
-import voluptuous as vol
-from homeassistant.helpers.typing import HomeAssistantType
-from homeassistant.config_entries import ConfigEntry
-
-from slack import WebClient
-from slack.errors import SlackApiError
-
 DOMAIN = "slack_user"
 COMPONENT_TYPES = ["sensor"]
 
@@ -17,7 +9,7 @@ async def async_setup(hass, config):
     return True
 
 
-async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
+async def async_setup_entry(hass, entry):
     """Set up Slack User Entry."""
     for component in COMPONENT_TYPES:
         hass.async_create_task(
